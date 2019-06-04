6f706fbcadf7a274cb0135843006d4caf4c07e83
Valentin Kaelin <kalanehd@gmail.com>
2019-06-04 11:05:28 +0200

    Edit User/Role migrations

--- database/migrations/2019_07_04_000000_create_users_table.php
+++ database/migrations/2019_07_04_000000_create_users_table.php
@@ -15,12 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('city');
+            $table->string('country');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
