49300f4ce8115b49684f15b24e7f2a84d5ed75c9
Valentin Kaelin <kalanehd@gmail.com>
2019-06-04 14:04:02 +0200

    Nullable Field User Model

--- database/migrations/2019_07_04_000000_create_users_table.php
+++ database/migrations/2019_07_04_000000_create_users_table.php
@@ -17,11 +17,11 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('city');
-            $table->string('country');
-            $table->string('email')->unique();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
