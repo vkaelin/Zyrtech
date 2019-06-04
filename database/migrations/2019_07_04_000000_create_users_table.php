a52824f7fcb88e71e675397ba130868942d999de
Valentin Kaelin <kalanehd@gmail.com>
2019-06-04 11:22:42 +0200

    Login/Register

--- database/migrations/2019_07_04_000000_create_users_table.php
+++ database/migrations/2019_07_04_000000_create_users_table.php
@@ -25,8 +25,8 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
 
-            $table->unsignedBigInteger('role_id');
-            $table->foreign('role_id')->references('id')->on('roles');
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null');
         });
     }
 
