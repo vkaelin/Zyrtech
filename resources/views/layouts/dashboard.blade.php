b2ef450a570a7ee7c6c838c4c3b1622f0d06f068
Valentin Kaelin <kalanehd@gmail.com>
2019-06-05 15:25:24 +0200

    Display all Chefs' Owner in a table

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,65 +13,67 @@
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body class="bg-gray-100 h-screen antialiased leading-none">
     <div id="app" class="flex flex-col h-full">
-        <nav class="bg-blue-900 shadow py-6">
-            <div class="container mx-auto px-6 md:px-0">
-                <div class="flex items-center justify-center">
-                    <div class="mr-6">
-                        <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
-                            {{ config('app.name', 'Laravel') }}
-                        </a>
-                    </div>
-                    <div class="flex-1 text-right">
-                        <a class="text-white" href="/">Retour au site</a>
-                        <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
 
-                        <a href="{{ route('logout') }}"
-                            class="no-underline hover:underline text-gray-300 text-sm p-3"
-                            onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">{{ __('Déconnexion') }}</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                            {{ csrf_field() }}
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-        <div class="flex h-full">
+        <div class="flex h-full overflow-hidden">
             {{-- Sidenav --}}
             <div class="w-64 p-4 bg-gray-200">
                 <h3 class="text-xl font-bold mb-4">Dashboard</h3>
                 <nav>
                     <ul>
                         <li class="font-bold py-2 hover:underline">
-                            <a
-                                href="/dashboard/chefs"
-                                class="{{ Request::is('dashboard/chefs') ? 'text-green-400' : ''}}"
-                            >
+                            <a href="/dashboard/chefs"
+                                class="{{ Request::is('dashboard/chefs') ? 'text-green-400' : ''}}">
                                 Chefs
                             </a>
                         </li>
                         @if (Request::is('dashboard/chefs*'))
-                            <li class="font-bold py-2 hover:underline pl-2">
-                                <a
-                                    href="/dashboard/chefs/create"
-                                    class="{{ Request::is('dashboard/chefs/create') ? 'text-green-400' : ''}}"
-                                >
-                                    Ajouter un Chef
-                                </a>
-                            </li>
+                        <li class="font-bold py-2 hover:underline pl-2">
+                            <a href="/dashboard/chefs/create"
+                                class="{{ Request::is('dashboard/chefs/create') ? 'text-green-400' : ''}}">
+                                Ajouter un Chef
+                            </a>
+                        </li>
                         @endif
                         <li class="font-bold py-2 hover:underline">
-                            <a href="/dashboard/sets" class="{{ Request::is('dashboard/sets') ? 'text-green-400' : ''}}">Sets</a>
+                            <a href="/dashboard/sets"
+                                class="{{ Request::is('dashboard/sets') ? 'text-green-400' : ''}}">Sets</a>
                         </li>
                     </ul>
                 </nav>
             </div>
             {{-- Main Content --}}
-            <div class="flex-1 p-4">
-                @yield('content')
+            <div class="flex-1">
+                {{-- Nav --}}
+                <nav class="bg-blue-900 shadow py-6">
+                    <div class="container mx-auto px-6 md:px-0">
+                        <div class="flex items-center justify-center">
+                            <div class="mr-6">
+                                <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
+                                    {{ config('app.name', 'Laravel') }}
+                                </a>
+                            </div>
+                            <div class="flex-1 text-right">
+                                <a class="text-white" href="/">Retour au site</a>
+                                <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
+
+                                <a href="{{ route('logout') }}"
+                                    class="no-underline hover:underline text-gray-300 text-sm p-3"
+                                    onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">{{ __('Déconnexion') }}</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                                    {{ csrf_field() }}
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                
+                <div class="p-4">
+                        @yield('content')
+                </div>
             </div>
         </div>
 
@@ -79,4 +82,5 @@
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
+
 </html>
--- resources/views/owner/chefs/index.blade.php
+++ resources/views/owner/chefs/index.blade.php
@@ -3,15 +3,52 @@
 @section('content')
 
 <div class="flex items-center mb-6">
-    <h1 class="text-xl font-bold">Les comptes de vos chefs</h1>
+    <h1 class="text-xl font-bold mr-4">Les comptes de vos chefs</h1>
     <a class="bg-green-400 rounded py-2 px-4 text-white" href="{{ url('/dashboard/chefs/create')}}">Ajouter un chef</a>
 </div>
 
-@forelse (auth()->user()->chefs as $chef)
-    <p>Prénom: {{ $chef->first_name }}</p>
-    <p class="mb-4">Nom: {{ $chef->last_name }}</p>
-@empty
-     <p>Vous n'avez pas de chefs.</p>
-@endforelse
+
+
+<div class="block w-full overflow-x-auto">
+    <table class="w-full text-left shadow-md bg-white rounded-lg">
+        <thead>
+            <tr>
+                <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-200">
+                    Prénom
+                </th>
+                <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-200">
+                    Nom
+                </th>
+                <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-200">
+                    Code
+                </th>
+                <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-200">
+                    Actions
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse (auth()->user()->chefs as $chef)
+            <tr class="hover:bg-gray-300">
+                <td class="py-4 px-6 border-b border-gray-200">{{ $chef->first_name }}</td>
+                <td class="py-4 px-6 border-b border-gray-200">{{ $chef->last_name }}</td>
+                <td class="py-4 px-6 border-b border-gray-200">Aucun code.</td>
+                <td class="py-4 px-6 border-b border-gray-200">
+                    <a href="#" class="text-gray-300 font-bold py-1 px-3 mr-2 rounded text-xs bg-green-600 hover:bg-green-700">
+                        Modifier
+                    </a>
+                    <a href="#" class="text-gray-300 font-bold py-1 px-3 rounded text-xs bg-red-600 hover:bg-red-700">
+                        Supprimer
+                    </a>
+                </td>
+            </tr>
+            @empty
+            <tr class="hover:bg-gray-300">
+                <td>Vous n'avez pas de chefs.</td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+</div>
 
 @endsection
