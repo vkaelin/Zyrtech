17c0f0638c9eb0c9ca94db01ecfc6f9474c642fb
Valentin Kaelin <kalanehd@gmail.com>
2019-06-05 16:18:56 +0200

    Installed vue-js-modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
 import './bootstrap';
 
 import Vue from 'vue';
+import VModal from 'vue-js-modal'
+
+Vue.use(VModal)
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('login-chef-modal', require('./components/LoginChefModal.vue').default);
 
 new Vue({
     el: '#app',
---
+++ resources/js/components/LoginChefModal.vue
@@ -0,0 +1,16 @@
+<template>
+  <modal name="login-chef" classes="p-10 bg-white rounded-lg" height="auto">
+    <h1 class="text-2xl font-bold mb-16 text-center">Entrez votre code</h1>
+
+  <form>
+    
+  </form>
+
+  </modal>
+</template>
+
+<script>
+export default {
+
+}
+</script>
