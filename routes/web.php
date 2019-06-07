f4b5123371ab014665461bf98b42f8eb5726cca0
Lazar <lazazr@gmail.com>
2019-06-07 08:42:17 +0200

    Fix routes for product attributes

--- routes/web.php
+++ routes/web.php
@@ -40,13 +40,29 @@ Route::group(['prefix' => 'admin',  'middleware' => 'auth'], function () {
     Route::patch('/products/{product}/edit', 'Admin\AdminProductController@update');
     Route::delete('/products/{product}', 'Admin\AdminProductController@destroy');
 
+    //Attributes
+    Route::get('/products/attributes', 'Admin\AdminAttributeController@index');
+
     //Types
-    Route::get('/attributes', 'Admin\AdminAttributeController@index');
-    Route::get('/attributes/create', 'Admin\AdminAttributeController@create');
-    Route::post('/attributes/', 'Admin\AdminAttributeController@store');
-    Route::get('/attributes/{attribute}/edit', 'Admin\AdminAttributeController@edit');
-    Route::patch('/attributes/{attribute}/edit', 'Admin\AdminAttributeController@update');
-    Route::delete('/attributes/{attribute}', 'Admin\AdminAttributeController@destroy');
+    Route::get('/products/attributes/types/create', 'Admin\AdminTypeController@create');
+    Route::post('/products/attributes/types/', 'Admin\AdminAttributeController@store');
+    Route::get('/products/attributes/types/{type}/edit', 'Admin\AdminTypeController@edit');
+    Route::patch('/products/attributes/types/{type}/edit', 'Admin\AdminTypeController@update');
+    Route::delete('/products/attributes/types/{type}', 'Admin\AdminTypeController@destroy');
+
+    //Periods
+    Route::get('/products/attributes/periods/create', 'Admin\AdminPeriodController@create');
+    Route::post('/products/attributes/periods/', 'Admin\AdminPeriodController@store');
+    Route::get('/products/attributes/periods/{period}/edit', 'Admin\AdminPeriodController@edit');
+    Route::patch('/products/attributes/periods/{period}/edit', 'Admin\AdminPeriodController@update');
+    Route::delete('/products/attributes/periods/{period}', 'Admin\AdminPeriodController@destroy');
+
+    //Labels
+    Route::get('/products/attributes/labels/create', 'Admin\AdminLabelController@create');
+    Route::post('/products/attributes/labels/', 'Admin\AdminLabelController@store');
+    Route::get('/products/attributes/labels/{label}/edit', 'Admin\AdminLabelController@edit');
+    Route::patch('/products/attributes/labels/{label}/edit', 'Admin\AdminLabelController@update');
+    Route::delete('/products/attributes/labels/{label}', 'Admin\AdminLabelController@destroy');
 });
 
 // Dashboard for Owners
