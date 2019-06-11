5a37b7ccc9c58676b2672f78ad182f425c9334ab
Valentin Kaelin <kalanehd@gmail.com>
2019-06-11 09:13:29 +0200

    Started DB Seeding

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'description' => $faker->text
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Set;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        // admin
+       factory(User::class, 1)->create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@doe.com',
+            'role_id' => 1,
+        ]);
+
+        // owner
+        factory(User::class, 1)->create([
+            'first_name' => 'Jane',
+            'last_name' => 'Doe',
+            'email' => 'jane@doe.com',
+            'role_id' => 2,
+        ]);
+
+        // chefs
+        User::all()->each(function ($user) {
+            $user->chefs()->saveMany(factory(App\User::class, 4)->make([
+                'role_id' => 3,
+            ]));
+        });
     }
 }
