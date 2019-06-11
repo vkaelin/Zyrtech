47fe5a4dccad788f6091d2661ce3dd029b5254e6
Lazar <lazazr@gmail.com>
2019-06-11 14:24:08 +0200

    Working on Stars in Product page

--- resources/js/components/ProductStar.vue
+++ resources/js/components/ProductStar.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="inline">
-        <i class="fas fa-star"></i>
+        
+        <i class="fas fa-star" 
+            v-for="(index) in starsNum" 
+            :key="index"
+            :class="[index <= stars ? 'text-green-400' : 'text-gray-400' ]"></i>    
     </div>
 </template>
 
 <script>
 export default {
     props:{
-        stars: Object
+        stars: Number,
+        readonly: Boolean
     },
     data() {
         return {
-            
+            starsNum : 5
         }
     },
 }
