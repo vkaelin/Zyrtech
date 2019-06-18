dc1178f408a499c7f3f9703ed554fc2bbf7bde29
Valentin Kaelin <kalanehd@gmail.com>
2019-06-18 10:49:31 +0200

    Enhanced Dashboard Search

--- resources/js/components/Shared/SearchDashboard.vue
+++ resources/js/components/Shared/SearchDashboard.vue
@@ -1,37 +1,51 @@
 <template>
-    <div class="relative flex">
-        <svg class="icon absolute left-0 ml-3 absolute-vert-center" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/></svg>
-        <input
-            v-model="value"
-            @input="search"
-            class="form-input pl-10"
-            type="text"
-            name="search"
-            autocomplete="off"
-            placeholder="Recherche..."
-        />
-        <button @click="reset" class="ml-2 bg-white flex items-center justify-center rounded px-4 py-3 leading-normal border border-gray-300 focus:outline-none hover:bg-gray-400">
-            <svg class="icon text-gray-800" viewBox="0 0 20 20"><path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/></svg>
-        </button>
-    </div>
+  <div class="relative flex">
+    <svg class="icon absolute left-0 ml-3 absolute-vert-center" viewBox="0 0 20 20">
+      <path
+        d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"
+      ></path>
+    </svg>
+    <input
+      v-model="value"
+      @input="search"
+      class="form-input px-10"
+      type="text"
+      name="search"
+      autocomplete="off"
+      placeholder="Recherche..."
+    >
+    <button v-show="!searchEmpty" @click="reset" class="mr-3 absolute right-0 absolute-vert-center focus:outline-none">
+      <svg class="icon text-gray-800" viewBox="0 0 20 20">
+        <path
+          d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"
+        ></path>
+      </svg>
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            value: ''
-        }
-    },
-
-    methods: {
-        reset() {
-            this.value = ''
-            this.search()
-        },
-        search() {
-            this.$emit('searching', this.value)
-        }
+  data() {
+    return {
+      value: ''
     }
+  },
+
+  computed: {
+      searchEmpty() {
+          return this.value.length === 0
+      }
+  },
+
+  methods: {
+    reset() {
+      this.value = ''
+      this.search()
+    },
+    search() {
+      this.$emit('searching', this.value)
+    }
+  }
 }
 </script>
