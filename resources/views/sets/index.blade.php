501668aeafcc6a0974909ac39c9a8427bf60dd58
Valentin Kaelin <kalanehd@gmail.com>
2019-06-21 12:36:02 +0200

    Fixed Chefs' Sets page

--- resources/views/sets/index.blade.php
+++ resources/views/sets/index.blade.php
@@ -8,9 +8,15 @@
     <h1 class="text-lg md:text-2xl font-bold mb-4">Vos Produits Assignés</h1>
     @endif
     <div class="flex flex-wrap">
+        @if ($set)
         @foreach ($set->products as $product)
         @include('partials.product-card', ['chef' => 'true'])
         @endforeach
+        @else
+        <div class="mt-2 text-red-500 italic text-sm">
+            Vous n'avez pas de set de produits assigné.
+        </div>
+        @endif
     </div>
 </div>
 
