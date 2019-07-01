394ab088f16f7d298baf37d6ea375e264db43fb4
Valentin Kaelin <kalanehd@gmail.com>
2019-07-01 17:02:59 +0200

    Remove old Heroku Stuff

--- config/database.php
+++ config/database.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$host = $url["host"] ?? null;
-$username = $url["user"] ?? null;
-$password = $url["pass"] ?? null;
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -21,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'heroku_mysql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -48,17 +42,6 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
-        'heroku_mysql' => array(
-            'driver' => 'mysql',
-            'host' => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-        ),
 
         'mysql' => [
             'driver' => 'mysql',
